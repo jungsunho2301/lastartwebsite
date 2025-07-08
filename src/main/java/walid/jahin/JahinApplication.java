bd8f125c51b57975d8137b949a9ec7bf7714def4
Jung Sun-Ho <jsh5472301@naver.com>
2025-07-08 18:48:39 +0900

    기본파일

---
+++ src/main/java/walid/jahin/JahinApplication.java
@@ -0,0 +1,13 @@
+package walid.jahin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JahinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JahinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/walid/jahin/JahinApplicationTests.java
@@ -0,0 +1,13 @@
+package walid.jahin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JahinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
