8b3cb8baf724366c8866275dd1d9abb0361587ea
Jung Sun-Ho <jsh5472301@naver.com>
2025-07-09 14:20:42 +0900

    .java파일 7개 추가

---
+++ src/main/java/walid/jahin/model/Artwork.java
@@ -0,0 +1,24 @@
+package walid.jahin.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Artwork {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    private String imagePath;
+}
---
+++ src/main/java/walid/jahin/model/SessionConst.java
@@ -0,0 +1,5 @@
+package walid.jahin.model;
+
+public class SessionConst {
+    public static final String LOGIN_ADMIN = "loginAdmin";
+}
